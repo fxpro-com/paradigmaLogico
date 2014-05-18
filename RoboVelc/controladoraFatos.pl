704dca15aaca29046c8a7ce3b8a91b4b56749cb9
Vanessa Barbosa <vanessabmdf@gmail.com>
2014-05-18 13:58:37 -0300

    Regra para inserir um novo Fato

--- RoboVelc/controladoraFatos.pl
+++ RoboVelc/controladoraFatos.pl
@@ -2,20 +2,23 @@
 %Manipula��o dos dados
 %remove uma tupla
 
-deleta(X):-
-	deleta1(X), fail.
-deleta(X). %garante que seja bem sucedido
+%deleta(X):-
+%	deleta1(X), fail.
+%deleta(X). %garante que seja bem sucedido
 
-deleta1(X):-
-	retract(X).
-deleta1(X). %garante que seja bem sucedido
+%deleta1(X):-
+%	retract(X).
+%deleta1(X). %garante que seja bem sucedido
 
 
 %grava(X):-
 	%esquece(X), assert(X).
 
-grava(X,Y,Z):-
-	assert(metodosNumericos(X,Y,Z)).
+%grava(X,Y,Z):-
+%	assert(metodosNumericos(X,Y,Z)).
 
 
 %Atividade: inserir e excluir dados da tabela de fatos
+
+inserirFatoPearson(Candles,Pearson):-append('tabelaFatos.pl'),
+	write('pearson('), write(Candles),write(','),write(Pearson), write(').'),nl,told.
