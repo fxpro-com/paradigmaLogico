eb3a92649457e83eb56c854a4c1b8ef7d28283a4
Cleiton Gomes <cleitoncsg@gmail.com>
2014-05-18 20:44:48 -0300

    Criação do arquivo MQL

--- RoboVelc/metodosNumericos.pl
+++ RoboVelc/metodosNumericos.pl
@@ -2,10 +2,26 @@
 :-include('tabelaFatos.pl').
 :-include('controladoraFatos.pl').
 
+%Esssa será a main
+
+
+
+
+
+
+
+
+
+
+
+
 
 %Tabela virtual: definir uma tabela sem ter que cri�-la
 %criterioDeEntrada(X,Y,Z):-metodosNumericos(X,_,Z).
-
+%metodoNinja(21,mmUltraRapida).
+%metodoNinja(34,mmMediaRapida).
+%metodoNinja(55,mmMedia).
+%metodoNinja(89,mmMediaLenta).
 
 
 
