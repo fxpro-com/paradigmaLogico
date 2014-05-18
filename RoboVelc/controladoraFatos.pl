3258645a4254955471922f9f0d6fd8ffa2f80c82
Cleiton Gomes <cleitoncsg@gmail.com>
2014-05-18 15:02:22 -0300

    ReconstroiTabela de fatos

--- RoboVelc/controladoraFatos.pl
+++ RoboVelc/controladoraFatos.pl
@@ -1,24 +1,24 @@
+:-include('tabelaFatos.pl').
+%pearson(21,0.8).
 
-%Manipula��o dos dados
-%remove uma tupla
-
-%deleta(X):-
-%	deleta1(X), fail.
-%deleta(X). %garante que seja bem sucedido
-
-%deleta1(X):-
-%	retract(X).
-%deleta1(X). %garante que seja bem sucedido
-
-
-%grava(X):-
-	%esquece(X), assert(X).
-
-%grava(X,Y,Z):-
-%	assert(metodosNumericos(X,Y,Z)).
-
-
-%Atividade: inserir e excluir dados da tabela de fatos
 
 inserirFatoPearson(Candles,Pearson):-append('tabelaFatos.pl'),
 	write('pearson('), write(Candles),write(','),write(Pearson), write(').'),nl,told.
+inserirFatoPearson(Candles,Pearson).
+
+reconstroiTabelaFatos():- reconstroiTabelaFatosAuxiliar(),fail, close(Arquivo).
+reconstroiTabelaFatos().
+
+reconstroiTabelaFatosAuxiliar():- open('tabelaFatos.pl',read,Arquivo),
+	read(Arquivo,Linha),
+	write(Linha).
+reconstroiTabelaFatosAuxiliar().
+
+
+%deleta(X):-
+	%deleta1(X), fail.
+%deleta(X).
+
+%deleta1(X):-
+	%retract(X).
+%deleta1(X).
